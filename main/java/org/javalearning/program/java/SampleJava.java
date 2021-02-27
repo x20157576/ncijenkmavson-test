8f7c6d347f1ce99ea88ec9f8e37719336da36850
x20157576 <x20157576@student.ncirl.ie>
2021-02-27 11:59:35 +0000

    first commit

---
+++ main/java/org/javalearning/program/java/SampleJava.java
@@ -0,0 +1,18 @@
+package org.javalearning.program.java;
+
+public class SampleJava {
+	
+	int x=5;
+	public int multiply(int num1, int num2) {
+		return num1*num2;
+	}
+	public int substract(int num1,int num2) {
+		try {
+			return num1;	
+		}
+		catch(Exception e) {
+			x = 7;
+		}
+		return num1-num2;
+	}
+}
